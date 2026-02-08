54152cfe230b4dd1b14354760129591becd33235
Jonathan Goulart <jonathasgoulart@gmail.com>
2026-02-08 20:58:03 -0300

    Cleanup: delete test files

--- app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestPage() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-black text-white">
-            <h1 className="text-4xl font-bold">Funciona! /test está ok.</h1>
-        </div>
-    )
-}
